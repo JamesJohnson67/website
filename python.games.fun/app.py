b56c2c0b20c666f3e700988914007896c6da1afb
JamesJohnson67 <James.Johnson@bostongrammarschool.co.uk>
2026-04-27 11:31:44 +0100

    added file

---
+++ python.games.fun/app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/")
+def bob():
+    return render_template("red pandas.html")
+@app.route("/projects/score-clicker")
+def score_clicker():
+    return render_template("score_clicker.html")
+if __name__ == "__main__":
+    app.run(debug=False)
